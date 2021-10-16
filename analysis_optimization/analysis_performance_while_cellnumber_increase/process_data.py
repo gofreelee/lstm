b6a4acce85b0d538b8a8a2f4554b1c39c4f2d811
gofreelee <1979432070@qq.com>
2021-10-16 14:28:25 +0800

    add 20cells analysis result

---
+++ analysis_optimization/analysis_performance_while_cellnumber_increase/process_data.py
@@ -0,0 +1,15 @@
+import csv
+
+
+csv_file =  open("4gemv_sum_20cells.csv")
+time_data = [n for n in range(119)]
+for i in csv_file:
+    row_data = i.split(',')
+    if len(row_data) == 9:
+        time_ = float(row_data[3])
+        func_type = row_data[6].split('_')[1]
+        func_number = row_data[6].split('_')[2].split('(')[0]
+        if func_type == 'compute':
+            time_data[int(func_number)] = time_
+for i in time_data:
+    print(i)
